7498a895dcc2e1b12ac75d497f1af95d3959ea7f
ptky <danielpataky2@gmail.com>
2026-03-25 11:19:03 +0100

    js update meg nem mukodik

--- egyéb/script.js
+++ egyéb/script.js
@@ -1,58 +1,29 @@
-let max = 0
-let max2 = 0
-let max3 = 0
-function like_add() {
-    if (max > 0) {
+let liked = {
+    1: false,
+    2: false,
+    3: false
+};
+function like_add(number) {
+    if (like_add(number))  {
         window.alert("Ezt már likeoltad!")
     }
     else {
-        max+=1
-        aktualis = document.getElementById("likeCount").innerText
-        document.getElementById("likeCount").innerText = parseInt(aktualis) + 1
-    }
-    
-    
-}
-function like_add2() {
-    if (max2 > 0) {
-        window.alert("Ezt már likeoltad!")
-    }
-    else {
-        max2+=1
-        aktualis = document.getElementById("likeCount2").innerText
-        document.getElementById("likeCount2").innerText = parseInt(aktualis) + 1
-    }
-    
-    
-}
-function like_add3() {
-    if (max3 > 0) {
-        window.alert("Ezt már likeoltad!")
-    }
-    else {
-        max3+=1
-        aktualis = document.getElementById("likeCount3").innerText
-        document.getElementById("likeCount3").innerText = parseInt(aktualis) + 1
+        let elem = document.getElementById(`likeCount${number}`);
+        let aktualis = parseInt(elem.innerText);
+        elem.innerText = aktualis + 1;
+        liked[number] = true
+
     }
     
     
 }
 
-function washington() {
-    let story = document.getElementById("washington_story")
-    story.style.display = "block";
-}
-function napoleon() {
-    let story = document.getElementById("napoleon_story")
+function open_story(adat) {
+    let story = document.getElementById(adat)
     story.style.display = "block";
 }
 
-function hide_story() {
-    let story = document.getElementById("washington_story")
-    story.style.display = "none";
-}
-
-function hide_story_napoleon() {
-    let story = document.getElementById("napoleon_story")
+function close_story(adat) {
+    let story = document.getElementById(adat)
     story.style.display = "none";
 }
